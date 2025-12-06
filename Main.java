54944621d1a6a55b92abee9feaf36c2249a3d86c
Ankesh Srivastava <ankeshsrivastava61289@gmail.com>
2025-12-06 10:33:13 +0530

    added Play again option

--- Main.java
+++ Main.java
@@ -1,46 +1,78 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        int min = 1;
-        int max = 100;
-        int tries = 0;
-        int randomNum = min + (int)(Math.random() * (max - min + 1));
+class GuessingGame {
+
+    private int min = 1;
+    private int max = 100;
+    private int randomNum;
+    private int attempts;
+
+    public void startGame() {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Enter your guess between 1 and 100 (or 0 to exit):");
-            int user_input = sc.nextInt();
+            resetGame();
+            System.out.println("🎯 Welcome to the Number Guessing Game!");
 
-            if (user_input == 0) {
-                System.out.println("Exiting...");
-                System.out.println("The number was: " + randomNum);
-                break;
+            while (true) {
+                System.out.print("Enter your guess between 1 and 100 (or 0 to exit): ");
+                int guess = sc.nextInt();
+
+                if (guess == 0) {
+                    System.out.println("Exiting...");
+                    System.out.println("The number was: " + randomNum);
+                    sc.close();
+                    return;
+                }
+
+                if (!isValidGuess(guess)) {
+                    System.out.println("Invalid input. Enter between 1 and 100.");
+                    continue;
+                }
+
+                attempts++;
+                if (checkGuess(guess)) break;
             }
 
-            if (user_input < 1 || user_input > 100)
-            {
-                System.out.println("Invalid input. Please enter a number between 1 and 100.");
-                continue;
-            }
+            System.out.println("You took " + attempts + " attempt(s).");
 
-            tries++; 
-            if (user_input < randomNum) 
-            {
-                System.out.println("You guessed too low.");
-            }
-            else if (user_input > randomNum) 
-            {
-                System.out.println("You guessed too high.");
-            }
-            else 
-            {
-                System.out.println(" You guessed it right!");
-                break;
+            System.out.print("Do you want to play again? (y/n): ");
+            char choice = sc.next().toLowerCase().charAt(0);
+
+            if (choice != 'y') {
+                System.out.println("Thanks for playing!");
+                sc.close();
+                return;
             }
         }
+    }
 
-        System.out.println("You took a total of " + tries + " attempt(s).");
-        sc.close();
+    private void resetGame() {
+        randomNum = min + (int)(Math.random() * (max - min + 1));
+        attempts = 0;
+    }
+
+    private boolean isValidGuess(int guess) {
+        return guess >= min && guess <= max;
+    }
+
+    private boolean checkGuess(int guess) {
+        if (guess < randomNum) {
+            System.out.println("Too low!");
+            return false;
+        } else if (guess > randomNum) {
+            System.out.println("Too high!");
+            return false;
+        } else {
+            System.out.println("🎉 You guessed it right!");
+            return true;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        GuessingGame game = new GuessingGame();
+        game.startGame();
     }
 }
